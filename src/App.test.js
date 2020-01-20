9c0b2c5ce6cbabdaf802ff7ab8d5f02e37f20585
Tona González <martiuh@gmail.com>
2020-01-20 07:33:51 -0600

    tests: change some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getAllByAltText, getAllByText } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import createStore from "createStore";
+import { Provider } from "react-redux";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("render", () => {
   let { store } = createStore({});
-  const { getByText } = render(<App store={store} />);
-  const linkElement = getByText(/home/i);
+  const { getAllByText } = render(
+    <Provider store={store}>
+      <App store={store} />
+    </Provider>
+  );
+  const linkElement = getAllByText(/home/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
